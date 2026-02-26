8e6833911dab88dea037de548a1151d45d285f3f
tpkee <89136677+tpkee@users.noreply.github.com>
2026-02-26 22:30:12 +0100

    fix: remove debug output from ItemSearch component

--- src/components/ItemSearch.vue
+++ src/components/ItemSearch.vue
@@ -8,7 +8,6 @@
     <div v-else class="w-8 h-12 bg-gray-700 rounded shrink-0 flex items-center justify-center">
       <Icon icon="mdi:image-off" class="text-gray-500 w-4 h-4" />
     </div>
-    xxx{{ result }}
 
     <div class="min-w-0">
       <p class="text-sm font-medium text-white truncate">{{ result.name }}</p>

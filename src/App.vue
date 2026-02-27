063e62ae03240edd69dd501bc1e9f55a237a9c43
tpkee <89136677+tpkee@users.noreply.github.com>
2026-02-27 03:49:45 +0100

    feat: add Suspense and animation and add custom loader

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,20 @@
     <router-view name="header" />
 
     <div class="container mx-auto p-4 pt-7!">
-      <router-view v-slot="{ Component, route }">
-        <transition name="fade">
-          <component :is="Component" :key="route.path" />
-        </transition>
+      <router-view v-slot="{ Component }">
+        <template v-if="Component">
+          <Transition mode="out-in">
+            <KeepAlive>
+              <Suspense>
+                <component :is="Component"></component>
+
+                <template #fallback>
+                  <app-loader />
+                </template>
+              </Suspense>
+            </KeepAlive>
+          </Transition>
+        </template>
       </router-view>
     </div>
   </main>
@@ -14,12 +24,14 @@
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+
+import AppLoader from '@/components/app/AppLoader.vue'
 </script>
 
 <style scoped>
 .v-enter-active,
 .v-leave-active {
-  transition: opacity 0.5s ease;
+  transition: opacity 0.15s ease-in-out;
 }
 
 .v-enter-from,
---
+++ src/components/app/AppLoader.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="fixed inset-0 w-screen bg-purple-600 z-999 h-px animate-pulse"
+    :style="{
+      width: `${width}vw`,
+      transition: 'width 0.2s ease-out',
+    }"
+  ></div>
+</template>
+
+<script lang="ts" setup>
+import { onBeforeUnmount, ref } from 'vue'
+
+const width = ref(0)
+
+let rafId: number | null = null
+const target = ref(0)
+
+function animate() {
+  console.log('Animating... Target:', target.value, 'Width:', width.value)
+  // increment target slowly and randomly
+  if (target.value < 100) {
+    target.value = Math.min(100, target.value + Math.random() * 0.3 + 0.05)
+  }
+
+  // smooth the actual width toward the target using linear interpolation
+  width.value += (target.value - width.value) * 0.1
+
+  if (width.value < 100) {
+    rafId = requestAnimationFrame(animate)
+  } else {
+    rafId = null
+  }
+}
+
+animate()
+
+onBeforeUnmount(() => {
+  if (rafId !== null) cancelAnimationFrame(rafId)
+})
+</script>

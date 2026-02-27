152eb98db1435aa4da033f24ede15fd3167c87d7
tpkee <89136677+tpkee@users.noreply.github.com>
2026-02-27 04:36:04 +0100

    feat: add VITE_BASE_PATH to deploy this thing to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
+  base: process.env.VITE_BASE_PATH || '/',
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })

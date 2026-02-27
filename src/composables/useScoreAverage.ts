34069b6b6bc83e143f885f5e17ea9c698c754391
tpkee <89136677+tpkee@users.noreply.github.com>
2026-02-27 04:27:46 +0100

    fix: update useScoreAverage to accept Nullable<number> for better type handling

--- src/composables/useScoreAverage.ts
+++ src/composables/useScoreAverage.ts
@@ -2,9 +2,9 @@ import { computed, toValue, type MaybeRefOrGetter } from 'vue'
 
 import { ScoreTreshold } from '@/utils/media/misc'
 
-export function useScoreAverage(score: MaybeRefOrGetter<number>) {
+export function useScoreAverage(score: MaybeRefOrGetter<Nullable<number>>) {
   return computed(() => {
-    const scoreValue = toValue(score)
+    const scoreValue = toValue(score) ?? 0
     if (scoreValue >= ScoreTreshold.GOOD) return 'text-green-500'
 
     if (scoreValue >= ScoreTreshold.AVERAGE) return 'text-yellow-500'

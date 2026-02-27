4609d9a5426dd9df745bfa6f73a544a8f183385f
tpkee <89136677+tpkee@users.noreply.github.com>
2026-02-27 02:06:05 +0100

    feat: add transition effects to router-view for smoother component changes

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
     <router-view name="header" />
 
     <div class="container mx-auto p-4 pt-7!">
-      <router-view />
+      <router-view v-slot="{ Component, route }">
+        <transition name="fade">
+          <component :is="Component" :key="route.path" />
+        </transition>
+      </router-view>
     </div>
   </main>
 </template>
@@ -11,3 +15,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 </script>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
